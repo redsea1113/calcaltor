065e070261002e07377970a937878dd3c8080f41
Ali Farhat <tfdc2003@gmail.com>
2023-06-24 08:11:04 +0400

    Initial Commit

---
+++ calcaltor/ContentView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var counter = 0
+    
+    var body: some View {
+        
+        VStack(alignment: .center){
+            
+            Text("Count \(counter)")
+                .font(.system(size: 50))
+                .fontWeight(.bold)
+                .foregroundColor(Color.blue)
+                .multilineTextAlignment(.leading)
+                .padding()
+            
+            Button {
+                counter += 1
+            } label: {
+                Text("Click Here")
+            }
+            .buttonBorderShape(.roundedRectangle(radius: 20))
+            .background(.yellow)
+            
+        }
+    }
+}
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
---
+++ calcaltor/calcaltorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calcaltorApp.swift
+//  calcaltor
+//
+//  Created by Ali Farhat on 06/06/2023.
+//
+
+//import SwiftUI
+//
+//@main
+//struct calcaltorApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
